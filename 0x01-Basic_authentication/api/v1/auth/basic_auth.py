6efaab48cbb7ed7edbf2a3841ade421c80d2d72a
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-11-13 17:40:00 +0100

    api/v1/auth/basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ Basic Authentication module
 """
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -64,3 +66,28 @@ class BasicAuth(Auth):
             return (None, None)
         email, password = decoded_base64_authorization_header.split(':', 1)
         return (email, password)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        Returns the User instance based on his email and password.
+
+        :param user_email: The user's email.
+        :param user_pwd: The user's password.
+        :return: A User instance or None if the credentials are invalid.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({'email': user_email})
+        if not users or len(users) == 0:
+            return None
+
+        user = users[0]  # Assuming email is unique, get the first match
+
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
