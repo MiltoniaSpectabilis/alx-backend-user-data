4e44484b404d3b29252249915579288edfa96ce4
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-11-13 16:33:25 +0100

    basic - base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,3 +12,18 @@ class BasicAuth(Auth):
 
     def __init__(self):
         super().__init__()
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str
+    ) -> str:
+        """
+        Extracts the Base64 part of the Authorization
+        header for Basic Authentication.
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split(" ")[1]
