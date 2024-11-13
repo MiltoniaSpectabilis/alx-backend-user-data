b0cd0fde385a86e9a8631ad7a9b1f818436cc4e5
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-11-13 17:08:59 +0100

    api/v1/auth/basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -47,3 +47,20 @@ class BasicAuth(Auth):
             return decoded_str
         except (base64.binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Extracts the user credentials from the Base64 decoded value.
+
+        :param decoded_base64_authorization_header: The decoded value.
+        :return: A tuple containing the email and password.
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return (email, password)
