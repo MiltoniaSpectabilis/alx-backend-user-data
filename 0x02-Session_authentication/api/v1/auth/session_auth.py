973d2671fb7ac0032c4668b91d18e46fd49e8891
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-11-15 23:03:42 +0100

    create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,10 +4,28 @@ Session Authentication Module
 """
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """
     Session Authentication Class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for a user_id.
+
+        Args:
+            user_id (str): The user ID for which to create a session.
+
+        Returns:
+            str: The created Session ID or None if the input is invalid.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
