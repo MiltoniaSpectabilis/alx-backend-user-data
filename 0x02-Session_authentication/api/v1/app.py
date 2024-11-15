b4977a33338c6a334484dca0fc20f29d8b4add54
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-11-16 00:05:21 +0100

    trying diff syntax

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -46,8 +46,9 @@ def before_request():
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None\
-            and auth.session_cookie(request) is None:
+    auth_header = auth.authorization_header(request)
+    session_cookie = auth.session_cookie(request)
+    if auth_header is None and session_cookie is None:
         abort(401)
 
     if auth.current_user(request) is None:
