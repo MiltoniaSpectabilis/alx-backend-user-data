c231c8c07587016768cc8658527c61cb5d15c484
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-11-15 22:57:52 +0100

    empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Session Authentication Module
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    Session Authentication Class
+    """
+    pass
